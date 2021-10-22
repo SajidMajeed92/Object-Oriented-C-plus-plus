ca5ca3c8751eddb56f992243f00b37b502999027
Sajid Majeed <sajidmajeed.ist@gmail.com>
2021-10-22 15:27:15 +0500

    First Program

---
+++ Hello_World.cpp
@@ -0,0 +1,11 @@
+// First Program using C++
+// Date : 29 September 2021
+// Course : Object Oriented Programming 
+
+#include <iostream> // Add Preprocesor Directives
+using namespace std;
+
+	int main() {
+	cout << "Start Pogramming with C++";
+	return 0;
+	}
